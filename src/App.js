e96e414382aaac1a8528b76ae4ea7d93cbadf894
maiiptw <pongthawat.m@gmail.com>
2020-08-09 21:48:02 +0700

    Ep.4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from 'Header';
+import Content from 'Content';
+import Footer from 'Footer';
 
 class App extends Component{
   render(){
@@ -7,9 +10,14 @@ class App extends Component{
       color : '#FF0000'
     }
     return (
+      // <div>
+      //   <h1 style={mystyle}>React Tutorial</h1>
+      //   <h2>{10000*10000}</h2>
+      // </div>
       <div>
-        <h1 style={mystyle}>React Tutorial</h1>
-        <h2>{10000*10000}</h2>
+        <Header/>
+        <Content/>
+        <Footer/>
       </div>
     );
   }
---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render(){
+        return(
+            <div>
+                <p>this is my content. Hello World</p>
+            </div>
+        );
+    }
+}
+export default Content;
